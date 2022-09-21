9b4224409c90f925330c4f75d696438262243119
Theo Gregory <tjmg@outlook.com>
2022-09-21 22:04:58 +0100

    feat: adds styled-components

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import _Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default function Document() {
   return (
@@ -10,4 +17,24 @@ export default function Document() {
       </body>
     </Html>
   )
+}
+
+export const getInitialProps = async (ctx: DocumentContext) => {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      })
+
+    const initialProps = await _Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [initialProps.styles, sheet.getStyleElement()],
+    }
+  } finally {
+    sheet.seal()
+  }
 }
